d2764471472d35ffd4fc9d9759c52df4852f7157
CharlotteXenia <126453593+CharlotteXenia@users.noreply.github.com>
2024-04-19 13:21:48 +0200

    media

--- vite-project/src/Components/foot.jsx
+++ vite-project/src/Components/foot.jsx
@@ -8,123 +8,111 @@ import { useEffect, useState } from "react";
 function Foot() {
     // javascript her
 
-
-    // "posts" kommer til at indeholde listen af møder
-    const [posts, setPosts] = useState([]);
-    /* "isPosts" kan være enten "true" (hvis der er møder), eller
-    "false" hvis ingen møder der er. */
-    const [isPosts, setIsPosts] = useState(true);
-
-
     const [navn, setNavn] = useState("");
     const [email, setEmail] = useState("");
     const [mobilnummer, setMobilnummer] = useState("");
     const [status, setStatus] = useState("");
-    const [dato, setDato]= useState("");
-    const [tidspunkt, setTidspunkt]= useState("");
-    const[indhold, setIndhold]= useState("");
-
+    const [dato, setDato] = useState("");
+    const [tidspunkt, setTidspunkt] = useState("");
+    const [indhold, setIndhold] = useState("");
 
     function handleSubmit(e) {
         e.preventDefault();
 
-            const url = 'https://baeredygtig-webdesign-default-rtdb.europe-west1.firebasedatabase.app/moeder.json';
+        const url = 'https://baeredygtig-webdesign-default-rtdb.europe-west1.firebasedatabase.app/moeder.json';
 
-            const formData = {
-                navn: navn,
-                email: email,
-                mobilnummer: mobilnummer,
-                dato: dato,
-                tidspunkt: tidspunkt,
-                indhold: indhold,
-                status: "Nyt møde"
-            };
+        const formData = {
+            navn: navn,
+            email: email,
+            mobilnummer: mobilnummer,
+            dato: dato,
+            tidspunkt: tidspunkt,
+            indhold: indhold,
+            status: "Nyt møde"
+        };
 
-            fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(formData),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Fejl ved gemning af formulardata');
+                }
+                setStatus('Møde booket.')
+                // console.log('Formulardata gemt i Firebase Realtime Database.');
             })
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error('Fejl ved gemning af formulardata');
-                    }
-                    setStatus('Møde booket.')
-                    // console.log('Formulardata gemt i Firebase Realtime Database.');
-                })
-                .catch((error) => {
-                    setStatus('Fejl ved booking', error)
-                    // console.error8'Fejl ved gemning af fomulardata', error);       
-                });
+            .catch((error) => {
+                setStatus('Fejl ved booking', error)
+                // console.error8'Fejl ved gemning af fomulardata', error);       
+            });
     }
 
-
-
     return (
-        <footer id="footer">      
-        
-            <div>  
-            <div className="kontakt"> 
-                <h3>Er du interesseret i at høre mere?</h3>
-                <a href="tel: +45 12345678"><img src={telefon} alt="telefon ikon" /> +45 12 34 56 78 </a> 
-                <a href="mailto: Kontakt@Plan.et.dk"><img src={mail} alt="mail ikon" /> Kontakt@Plan.et.dk </a>
-                <a href="#"><img src={hus} alt="hus ikon" /> Planetvej 1, Earth 0000</a>
-            </div>
-            <div className="formular">
-            <form onSubmit={handleSubmit}>
-                <input value={navn}
-                onChange={(e)=>setNavn(e.target.value)}
-                    type="text"
-                    name="navn"
-                    placeholder="Dit Navn"
-                    required
-                />
-                <input value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-                    type="email"
-                    name="email"
-                    placeholder="Din Email"
-                    required
-                />
-                <input value={mobilnummer}
-                onChange={(e)=>setMobilnummer(e.target.value)}
-                    type="tel"
-                    name="mobilnummer"
-                    placeholder="Dit Mobilnummer"
-                    required
-                />
-                <input value={dato}
-                onChange={(e)=>setDato(e.target.value)}
-                    type="date"
-                    name="Dato"
-                    placeholder="dato for møde"
-                    required
-                />
-                <input value={tidspunkt}
-                onChange={(e)=>setTidspunkt(e.target.value)}
-                    type="time"
-                    name="Tidspunkt"
-                    placeholder="tidspunkt for møde"
-                    required
-                />
-                <textarea id="meetinginfo" value={indhold}
-                onChange={(e)=>setIndhold(e.target.value)}
-                    type="text"
-                    name="indhold"
-                    placeholder="Hvad skal mødet handle om?"
-                    required
-                />
-                {status}
-                <button type="submit">Book møde</button>
-            </form>
-            </div>
+        <footer id="footer">
+
+            <div>
+                <div className="kontakt">
+                    <h3>Er du interesseret i at høre mere?</h3>
+                    <a href="tel: +45 12345678"><img src={telefon} alt="telefon ikon" /> +45 12 34 56 78 </a>
+                    <a href="mailto: Kontakt@Plan.et.dk"><img src={mail} alt="mail ikon" /> Kontakt@Plan.et.dk </a>
+                    <a href="#"><img src={hus} alt="hus ikon" /> Planetvej 1, Earth 0000</a>
+                </div>
+                <div className="formular">
+                    <form onSubmit={handleSubmit}>
+                        <input value={navn}
+                            onChange={(e) => setNavn(e.target.value)}
+                            type="text"
+                            name="navn"
+                            placeholder="Dit Navn"
+                            required
+                        />
+                        <input value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            name="email"
+                            placeholder="Din Email"
+                            required
+                        />
+                        <input value={mobilnummer}
+                            onChange={(e) => setMobilnummer(e.target.value)}
+                            type="tel"
+                            name="mobilnummer"
+                            placeholder="Dit Mobilnummer"
+                            required
+                        />
+                        <input value={dato}
+                            onChange={(e) => setDato(e.target.value)}
+                            type="date"
+                            name="Dato"
+                            placeholder="dato for møde"
+                            required
+                        />
+                        <input value={tidspunkt}
+                            onChange={(e) => setTidspunkt(e.target.value)}
+                            type="time"
+                            name="Tidspunkt"
+                            placeholder="tidspunkt for møde"
+                            required
+                        />
+                        <textarea id="meetinginfo" value={indhold}
+                            onChange={(e) => setIndhold(e.target.value)}
+                            type="text"
+                            name="indhold"
+                            placeholder="Hvad skal mødet handle om?"
+                            required
+                        />
+                        {status}
+                        <button type="submit">Book møde</button>
+                    </form>
+                </div>
             </div>
         </footer>
         // alt html her (inden for forældre <>)
-      )
-    }
-    
-    export default Foot
-    
+    )
+}
+
+export default Foot
--- vite-project/src/Pages/forside.jsx
+++ vite-project/src/Pages/forside.jsx
@@ -1,7 +1,6 @@
 import Greenhushwash from '../Components/greenhushwash'
 import Landing from '../Components/landing'
 import Lefttext from '../Components/lefttext'
-import Righttext from '../Components/righttext'
 import Imgleft from '../Components/imgleft'
 import Imgright from '../Components/imgright'
 import Textboks from '../Components/textboks'
@@ -21,61 +20,60 @@ function Forside() {
   const scrollToFooter = () => {
     const footer = document.getElementById('footer');
     console.log("virker1");
-      footer.scrollIntoView({ behavior: 'smooth' });
-      console.log("virker");
-    
+    footer.scrollIntoView({ behavior: 'smooth' });
+    console.log("virker");
   };
 
-// javascript her
+  // javascript her
 
   return (
     <>
-    <Landing scrollToFooter={scrollToFooter}>
-    </Landing>
+      <Landing scrollToFooter={scrollToFooter}>
+      </Landing>
 
-    <Textboks overskrift="Hvad tilbyder vi?" 
-    tekst="Hos Plan.et tilbyder vi skræddersyede hjemmesideløsninger med fokus på bæredygtigt webdesign. Vores dedikerede team af webdesignere og udviklere stræber efter at skabe smukke og funktionelle hjemmesider, der overholder principperne om miljømæssig bæredygtighed." 
-    tekst2="Velkommen til Plan.et, hvor vi prioriterer både din online succes og vores planets fremtid.">
+      <Textboks overskrift="Hvad tilbyder vi?"
+        tekst="Hos Plan.et tilbyder vi skræddersyede hjemmesideløsninger med fokus på bæredygtigt webdesign. Vores dedikerede team af webdesignere og udviklere stræber efter at skabe smukke og funktionelle hjemmesider, der overholder principperne om miljømæssig bæredygtighed."
+        tekst2="Velkommen til Plan.et, hvor vi prioriterer både din online succes og vores planets fremtid.">
 
-    </Textboks>
+      </Textboks>
 
-    <Lefttext overskrift="Bæredygtigt webdesign" 
-    tekst="Vidste du, at internettet producerer 1,7 milliarder ton CO2 årligt? " 
-    tekst2="Det svarer til lidt over 3% af verdens samlede CO2-udledning, hvilket gør vores online færden lige så belastende for klimaet som verdens samlede flytrafik." 
-    animationData={flyinternet}>
+      <Lefttext overskrift="Bæredygtigt webdesign"
+        tekst="Vidste du, at internettet producerer 1,7 milliarder ton CO2 årligt? "
+        tekst2="Det svarer til lidt over 3% af verdens samlede CO2-udledning, hvilket gør vores online færden lige så belastende for klimaet som verdens samlede flytrafik."
+        animationData={flyinternet}>
 
-    </Lefttext>
+      </Lefttext>
 
-    <Textboks overskrift="Internettet udleder også CO2"
-    tekst="Selvom det måske ikke er åbenlyst, bidrager internettet også til CO2-udledningen. Hos vores webbureau er vi opmærksomme på denne påvirkning, og vi arbejder på at minimere vores digitale fodaftryk gennem bæredygtigt webdesign. Ved at vælge effektive designprincipper og miljøvenlige teknologier stræber vi efter at skabe hjemmesider, der ikke kun er funktionelle, men også skånsomme mod miljøet."></Textboks>
-    <div style={{backgroundImage:`url(${textureImage})`}}>
+      <Textboks overskrift="Internettet udleder også CO2"
+        tekst="Selvom det måske ikke er åbenlyst, bidrager internettet også til CO2-udledningen. Hos vores webbureau er vi opmærksomme på denne påvirkning, og vi arbejder på at minimere vores digitale fodaftryk gennem bæredygtigt webdesign. Ved at vælge effektive designprincipper og miljøvenlige teknologier stræber vi efter at skabe hjemmesider, der ikke kun er funktionelle, men også skånsomme mod miljøet."></Textboks>
+      <div style={{ backgroundImage: `url(${textureImage})` }}>
 
-    <Imgleft overskrift="Tyskland" 
-    tekst="Tyskland udleder 2.17% af den globale udledning af CO2, og er derfor nummer seks på listen over de lande der udleder mest. "
-    animationData={tyskland}
-    ></Imgleft>
-  
-      <Imgright overskrift="Kina" 
-      tekst="Kina udleder 29.18% af den globale udledning af CO2 og ligger derfor nummer et på listen over de lande der udleder mest."
-      animationData={kina}
-      ></Imgright>
+        <Imgleft overskrift="Tyskland"
+          tekst="Tyskland udleder 2.17% af den globale udledning af CO2, og er derfor nummer seks på listen over de lande der udleder mest. "
+          animationData={tyskland}
+        ></Imgleft>
 
-      <Imgleft overskrift="Internettet" 
-      tekst="Den mængde CO2 som internettet og it branchen udleder vil altså svare til 3.51% af den globale udledning af CO2. Det vil sige, at hvis internettet var et land, så ville det stjæle Tysklands position på 6. pladsen"
-      animationData={internetpit}
-      ></Imgleft>
+        <Imgright overskrift="Kina"
+          tekst="Kina udleder 29.18% af den globale udledning af CO2 og ligger derfor nummer et på listen over de lande der udleder mest."
+          animationData={kina}
+        ></Imgright>
 
-      <Greenhushwash></Greenhushwash>
-    </div>
-    <Anmeldelse 
-    overskrift= "Anmeldelser"
-    tekst='"Plan.et leverede en fremragende service til min lille virksomhed med deres fokus på bæredygtigt webdesign. Resultatet var både smukt og funktionelt, og jeg satte pris på den positive miljøpåvirkning, vores nye hjemmeside havde. Jeg kan varmt anbefale Plan.et til andre små virksomheder, der ønsker at gøre en forskel online."'
-    tekst2="- Kunde ejer, djaiosd"
-    src={anmeldelsesbillede}
-    alt="anmeldelses billede af plante med cirkler"></Anmeldelse>
+        <Imgleft overskrift="Internettet"
+          tekst="Den mængde CO2 som internettet og it branchen udleder vil altså svare til 3.51% af den globale udledning af CO2. Det vil sige, at hvis internettet var et land, så ville det stjæle Tysklands position på 6. pladsen"
+          animationData={internetpit}
+        ></Imgleft>
+
+        <Greenhushwash></Greenhushwash>
+      </div>
+      <Anmeldelse
+        overskrift="Anmeldelser"
+        tekst='"Plan.et leverede en fremragende service til min lille virksomhed med deres fokus på bæredygtigt webdesign. Resultatet var både smukt og funktionelt, og jeg satte pris på den positive miljøpåvirkning, vores nye hjemmeside havde. Jeg kan varmt anbefale Plan.et til andre små virksomheder, der ønsker at gøre en forskel online."'
+        tekst2="- Kunde ejer, djaiosd"
+        src={anmeldelsesbillede}
+        alt="anmeldelses billede af plante med cirkler"></Anmeldelse>
 
 
-  <Foot></Foot>
+      <Foot></Foot>
     </>
     // alt html her (inden for forældre <>)
   )
